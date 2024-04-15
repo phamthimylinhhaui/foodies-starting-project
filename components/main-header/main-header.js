491cf475eb715bbbf482e60c10eedfe5846a7b22
hapo-linhptm <linh.ptm@haposoft.com>
2024-04-15 23:01:46 +0700

    lesson-105: add content community page add active nav url

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
 
 export default function MainHeader() {
     return (
@@ -15,8 +16,12 @@ export default function MainHeader() {
                 </Link>
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href="/meals">Browse Meals</Link></li>
-                        <li><Link href="/community">Foodies community</Link></li>
+                        <li>
+                            <NavLink href="/meals">Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href="/community">Foodies community</NavLink>
+                        </li>
                     </ul>
                 </nav>
             </header>
